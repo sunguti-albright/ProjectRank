0b2d9fd4fa9f4a8c1ff07497008891dd47bbad38
albright <sungutialbright@gmail.com>
2022-06-14 05:23:15 +0300

    style: add homepage and project styling and background image

---
+++ static/js/script.js
@@ -0,0 +1,18 @@
+// SHOW MODAL
+$(document).ready(function(){
+    $('#modal-btn').click(function(){
+        $('.ui.modal')
+        .modal('show')
+    });
+
+});
+
+// REMOVE MESSAGE
+$('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
