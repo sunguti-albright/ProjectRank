0d2f8c8e9c272830c72dcd430119b91063cdf8d3
albright <sungutialbright@gmail.com>
2022-06-14 04:41:09 +0300

    add url patterns for search and update functions

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -1,16 +1,21 @@
-from django.urls import path 
+from django.urls import re_path, path , include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import my_profile_view,home,reviewPhoto,search_results,delete_project,update_project
-from awardsapp import views
+from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView 
+from .models import Profile, User, Post
 
 urlpatterns = [
     path('myprofile/',my_profile_view,name ='profile'),
     path('' , home , name='home'),
+    path('reviewPost/<str:pk>' , reviewPhoto , name='review'),
+    path('search/',search_results, name ='search_results'),
+    path('delete/<int:pk>',delete_project, name = 'deletepost'), 
+    path('update/<str:pk>',update_project, name = 'updatepost'), 
+    re_path(r'ratings/', include('star_ratings.urls', namespace='ratings')),
     
-
-
-
 ]
 
 if settings.DEBUG:
