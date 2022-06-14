f82ef0a575a14500d0da6601251cf5380ac306a1
albright <sungutialbright@gmail.com>
2022-06-14 04:46:34 +0300

    import signals to the app

---
+++ awardsapp/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class AwardsappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'awardsapp'
+    
+    def ready(self):
+        import awardsapp.signals
+
