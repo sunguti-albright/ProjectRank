c37f7301be5d8b05db2d84d6acfb3667de3ef9b6
albright <sungutialbright@gmail.com>
2022-06-14 04:34:27 +0300

    ft: add view for profile

---
+++ awardsapp/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import redirect, render
+from awardsapp.models import Profile,Post
+from awardsapp.forms import ProfileModelForm,PostModelForm
+from django.contrib.auth.decorators import login_required
+import datetime as dt
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import *
+from .models import *
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import *
+from .permissions import IsAdminOrReadOnly
+from django.http import JsonResponse
+from rest_framework import status
+from .permissions import IsAdminOrReadOnly
+
+
+# Create your views here.
+# @login_required(login_url='/accounts/login/')
+
+def my_profile_view(request):
+    profile = Profile.objects.get(user = request.user)
+    posts = Post.objects.filter(author=request.user).order_by('-created')
+    form = ProfileModelForm(request.POST or None ,request.FILES or None,instance = profile)
+    p_form = PostModelForm(request.POST or None,request.FILES or None)
+
+    comfirm = False
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            comfirm = True
+
+            
+    if p_form.is_valid():
+        instance = p_form.save(commit=False)
+        instance.author = request.user
+        instance.save()
+        return redirect('home')
+    p_form = PostModelForm()            
+         
+    context = {
+        'profile':profile,
+        'form':form,
+        'comfirm':comfirm,
+        'posts':posts,
+        'p_form':p_form,
+
+
+    }
+    return render(request,'profiles/myprofile.html',context)
+
+def home(request):
+    posts = Post.objects.all()
+    date = dt.date.today()
+
+    context = {
+        'posts':posts,
+        'date':date
+    }
+
+    return render(request,'awards/home.html',context)
+
+@login_required(login_url='/accounts/login/')
+def reviewPhoto(request,pk):
+    post = Post.objects.get(id = pk)
+    context = {
+        'post':post
+
+    }
+
+
+    return render(request,'awards/review.html',context)
+    
